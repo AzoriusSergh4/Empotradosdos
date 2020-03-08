62e3ce8e46b447a994e366de6cbc13e64c7175f3
JesusBorrero <shyfu98@gmail.com>
2020-03-08 17:26:57 +0100

    Post Controllers added

---
+++ Practica1SD/src/main/java/practica1/GeneralController.java
@@ -0,0 +1,46 @@
+package practica1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import practica1.author.Author;
+import practica1.author.AuthorRepository;
+import practica1.client.Client;
+import practica1.client.ClientRepository;
+import practica1.picture.Picture;
+import practica1.picture.PictureRepository;
+import practica1.picture.soldPicture.SoldPicture;
+import practica1.picture.soldPicture.SoldPictureRepository;
+
+import javax.annotation.PostConstruct;
+import java.sql.Date;
+
+@Controller
+public class GeneralController {
+
+    @Autowired
+    private SoldPictureRepository soldPictureRepository;
+
+    @Autowired
+    private PictureRepository pictureRepository;
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @PostConstruct
+    public void init() {
+        Author author = new Author("José", "Pérez", "22222222J", 1972, "Ecuador",
+                "Av. 6 de Diciembre, Quito 170505, Ecuador", "pintorjoseperez@gmail.com", "5932498124");
+        authorRepository.save(author);
+
+        Client buyer = new Client("Fernado", "López", "11111111H", "Calle Tulipán, s/n, 28933 Móstoles, Madrid",
+                "compradorfernandolopez@gmail.com", "678912354");
+        clientRepository.save(buyer);
+
+        Picture cuadro = new SoldPicture("Bacon", "Un cuadro de un crujiente bacon frito",
+                2017, 30.52, 26.88, 3000, author, buyer, new Date(12/01/2020));
+        pictureRepository.save(cuadro);
+    }
+}
--- Practica1SD/src/main/java/practica1/picture/PictureController.java
+++ Practica1SD/src/main/java/practica1/picture/PictureController.java
@@ -1,4 +1,19 @@
 package practica1.picture;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/picture")
 public class PictureController {
+    @Autowired
+    private PictureRepository pictureRepository;
+
+    @PostMapping("/")
+    public void addPicture(@RequestBody Picture picture) {
+        this.pictureRepository.save(picture);
+    }
 }
