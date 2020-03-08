cc7c4fa2ce3286d355b34d11286dd2dd1ee7a79b
JesusBorrero <shyfu98@gmail.com>
2020-03-08 17:47:59 +0100

    Application Update

--- Practica1SD/src/main/java/practica1/Practica1SdApplication.java
+++ Practica1SD/src/main/java/practica1/Practica1SdApplication.java
@@ -2,9 +2,10 @@ package practica1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class Practica1SdApplication {
+public class Practica1SdApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Practica1SdApplication.class, args);
