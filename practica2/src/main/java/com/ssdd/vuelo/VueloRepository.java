9470d837a9c1e6001b1a003b3d10a6c0715173f0
AzoriusSergh4 <43406514+AzoriusSergh4@users.noreply.github.com>
2020-04-25 12:07:20 +0200

    busqueda aprcial vuelos

--- practica2/src/main/java/com/ssdd/vuelo/VueloRepository.java
+++ practica2/src/main/java/com/ssdd/vuelo/VueloRepository.java
@@ -1,9 +1,11 @@
 package com.ssdd.vuelo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface VueloRepository extends JpaRepository<Vuelo, Long> { 
 
-
+	List<Vuelo> findByOrigenNombreContainingIgnoreCase(String nombre);
 }
--- practica2/src/main/java/com/ssdd/vuelo/VueloRestController.java
+++ practica2/src/main/java/com/ssdd/vuelo/VueloRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/vuelos")
@@ -16,6 +17,11 @@ public class VueloRestController {
 	@RequestMapping("/")
 	public List<Vuelo> listVuelos(){
 		return vueloRepository.findAll();
+	}
+	
+	@RequestMapping("/find")
+	public List<Vuelo> findVuelos(@RequestParam String nombre){
+		return vueloRepository.findByOrigenNombreContainingIgnoreCase(nombre);
 	}
 	
 }
