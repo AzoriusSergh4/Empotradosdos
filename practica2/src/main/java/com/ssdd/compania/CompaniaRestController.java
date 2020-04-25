bc7f703e4dd2a4600808fdcf96da977d220a2412
Sara <37405568+srodriguezal@users.noreply.github.com>
2020-04-25 11:36:34 +0200

    CompaniaController hecho

--- practica2/src/main/java/com/ssdd/compania/CompaniaRestController.java
+++ practica2/src/main/java/com/ssdd/compania/CompaniaRestController.java
@@ -1,5 +1,25 @@
 package com.ssdd.compania;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RequestMapping("/companias")
+@RestController
 public class CompaniaRestController {
+	
+	@Autowired
+	CompaniaRepository companiaRepository;
+
+	@CrossOrigin
+	@RequestMapping("/")
+	public List<Compania> listCompania() {
+		return companiaRepository.findAll();
+	}
 
 }
